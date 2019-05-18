2c426d4eeaf5b1455ab95c60789257521ee28c23
nolan <nolansplan@gmail.com>
2019-05-18 16:56:57 -0500

    refactor map initialization

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path    = require('path');
 const app     = express();
 const port    = process.env.PORT || 3000;
+// const apiKey  = process.env.APIKEY;
+
 
 app.use(express.static('public'));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
--- public/index.js
+++ public/index.js
@@ -1,54 +1,106 @@
-let lat;
-let long;
-navigator.geolocation.getCurrentPosition((res) => {
-    let lat = res.coords.latitude;
-    let long = res.coords.longitude;
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=c48566402cc12bee720fb0f4c5585055`;
-    fetch(url).then((res) => {
-        return res.json();
-    }).then((json) => {
-        if (json.weather[0].main == "Rain") {
-            $('.answer').text("Yes");
-        } else {
-            $('.answer').text("No");
-        }
-    })
-})
-
 var map;
-var myLatlng;
+var acceleration = 0.00098;
+var nDrops = 4000;
+var drops = [];
 function initMap() {
     navigator.geolocation.getCurrentPosition((res) => {
-        let lat = res.coords.latitude;
-        let long = res.coords.longitude;
-        myLatlng = {lat: lat, lng: long};
+        var myLatLng = setLatLng(res);
         map = new google.maps.Map(document.getElementById('map'), {
-            center: myLatlng,
+            center: myLatLng,
             zoom: 8
         });
         var marker = new google.maps.Marker({
-            position: myLatlng,
+            position: myLatLng,
             map: map,
         });
         map.addListener('click', (e) => {
-            $('.city-name').remove();
-            newLat = e.latLng.lat()
-            newLong = e.latLng.lng()
-            newLatlng = {lat: newLat, lng: newLong};
-            marker.setPosition(newLatlng);
-            map.setCenter(marker.position);
-            let url = `https://api.openweathermap.org/data/2.5/weather?lat=${newLat}&lon=${newLong}&appid=c48566402cc12bee720fb0f4c5585055`;
-            fetch(url).then((res) => {
-                return res.json();
-            }).then((json) => {
-                var name = json.name
-                var nameDiv = $(`<div class=city-name>${name}</div>`)
-                if (json.weather[0].main == "Rain") {
-                    $('.answer').append(nameDiv)
-                } else {
-                    $('.answer').append(nameDiv)
-                }
-            });
+            setMapPosition(e, marker);
         })
+
+        getWeatherData(myLatLng);
     })
+    function setMapPosition(e, marker) {
+        var newLatlng = { 
+            lat: e.latLng.lat(), 
+            lng: e.latLng.lng() 
+        };
+        marker.setPosition(newLatlng);
+        map.setCenter(marker.position);
+        getWeatherData(newLatlng);
+    }
+
+    function setLatLng(data) {
+        return {
+            lat: data.coords.latitude,
+            lng: data.coords.longitude
+        }
+    }
+
+    function getWeatherData(coords) {
+        let url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords["lat"]}&lon=${coords["lng"]}&appid=c48566402cc12bee720fb0f4c5585055`;
+        fetch(url).then((res) => {
+            return res.json();
+        }).then((json) => {
+            renderResponse(json);
+        });
+    }
+
+    function renderResponse(json) {
+        var name = json.name;
+        var nameDiv = $(`<div class=city-name>${name}</div>`);
+        $('.title').text(`Is it raining in ${name}?`);
+        console.log(json);
+        json.weather[0].main == "Rain" ? $('.answer').text("Yes") : $('.answer').text("No");
+    }
+}
+
+// p5 sketch
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0)
+  canvas.style('z-index', '-1');
+  for (i = 0; i < nDrops; i++) {
+    drops.push(new Drop());
+  }
+}
+
+function draw() {
+  clear();
+  background(230, 230, 250);
+  drops.forEach(function(d) {
+    d.draw();
+    d.fall();
+  });
+}
+
+function Drop() {
+  this.initX = function() {
+    this.x = random() * width;
+  };
+  this.initY = function() {
+    this.y = -random() * height;
+  };
+
+  this.initX();
+  this.y = random() * height;
+
+  this.length = random(1, 20);
+  this.speed = random();
+
+  this.draw = function() {
+    stroke(25, 105, 234);
+    line(this.x, this.y, this.x, this.y + this.length)
+  };
+
+  this.fall = function() {
+    if (this.y < height) {
+      this.speed += acceleration * this.length;
+      this.y += this.speed;
+      // make it appear that smaller drops are farther away
+    } else {
+      this.speed = random();
+      this.initY();
+      this.initX();
+    }
+  };
 }
